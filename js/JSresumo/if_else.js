97fb64f6a5cc3fc3e8642ec2e9a7a5c11d602b35
Gustavo <gustavo.souza.melo@hotmail.com>
2025-11-06 18:01:15 -0300

    if_else-JS

---
+++ js/JSresumo/if_else.js
@@ -0,0 +1,9 @@
+let idade = prompt("digite sua idade")
+        if( (idade > 18) || (idade ==18)) {
+            alert("vc e maior de idade")
+        }
+        
+        else {
+            alert("vc e menor de idade")
+        }
+        
