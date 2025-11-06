a16b11d464d0a751aee8ed48a4b08576117d1ee5
Gustavo <gustavo.souza.melo@hotmail.com>
2025-11-06 17:53:18 -0300

    eventosDOM-cores

---
+++ js/6_eventosDOM/index.js
@@ -0,0 +1,15 @@
+function clicar(){
+            principal.style.background = 'pink'
+            principal.innerText =  'clicou'
+            principal.style.color = 'white'
+        }
+        function entrar(){
+            principal.style.background = 'blue'
+            principal.style.color = 'violet'
+            principal.innerText = 'entrou'
+        }
+        function sair(){
+            principal.style.background = 'green'
+            principal.style.color = 'red'
+            principal.innerText = 'saiu'
+        }
