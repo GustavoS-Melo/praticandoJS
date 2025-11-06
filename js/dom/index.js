3f82c47d071c9725d10c1a3828bfd7342a288db2
Gustavo <gustavo.souza.melo@hotmail.com>
2025-11-06 17:54:37 -0300

    eventosDOM-background

---
+++ js/dom/index.js
@@ -0,0 +1,7 @@
+//window.document.write('Minha primeira manipulação dom')
+//window.alert('oi')
+let x = window.document.getElementById('principal');
+x.style.background = 'black';
+
+let y = window.document.getElementsByTagName('button')[1];
+y.innerHTML = 'caio';
