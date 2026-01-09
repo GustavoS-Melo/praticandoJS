86f13f279e9420cc3450ac86bd3d1d0b1019c209
Gustavo <gustavo.souza.melo@hotmail.com>
2026-01-09 14:43:12 -0300

    particando logica

---
+++ js udemy/ex logica 01/index.js
@@ -0,0 +1,9 @@
+const num1 = 5;
+const num2 = 10;
+
+function maiorNum(){
+    if(num1 > num2){console.log(`${num1} é maior que ${num2}`)} 
+    else{console.log(`${num2} é maior que ${num1}`)}
+}
+
+maiorNum()
